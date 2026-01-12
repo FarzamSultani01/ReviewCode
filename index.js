f35ac7623a13cfeeccaffeb19e3d4cd9958a0287
farzamsultani <farzamsaber03@gmail.com>
2026-01-12 21:41:34 +0430

    update

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-console.log("JavaScript is linked properly.");
+// console.log("JavaScript is linked properly.");
+
+
+//==============Working on MAP, Reduce, Filter==================//
+
+//EX 1 const numbers=[1,2,3,4];
+// const dub= numbers.map(num => num*2)
+// alert(dub);
+
+// EX2
+
+const numbers =[4,6,8,9];
+const min = numbers.map(mins => mins -3);
+alert(min)
